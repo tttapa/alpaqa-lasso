889ab3ad5036b29fe97b3133ec09788664b36e7c
Pieter Pas <pieter.pas@outlook.com>
2023-12-17 22:01:41 +0100

    More datasets

--- generate-data.py
+++ generate-data.py
@@ -1,12 +1,18 @@
 import numpy as np
-from sklearn.datasets import load_breast_cancer
+import sklearn.datasets
 
-X, y = load_breast_cancer(return_X_y=True)
-print("Number of instances:", X.shape[0])
-print("Number of attributes:", X.shape[1])
-assert y.shape[0] == X.shape[0]
-name = "breast_cancer"
-with open(f"{name}.csv", "w") as f:
-    np.savetxt(f, [X.shape], delimiter=",", newline="\n", fmt="%d")
-    np.savetxt(f, [y], delimiter=",", newline="\n")
-    np.savetxt(f, X.T, delimiter=",", newline="\n")
+for name in ("iris", "diabetes", "digits", "linnerud", "wine", "breast_cancer"):
+    load = getattr(sklearn.datasets, f"load_{name}")
+    X, y = load(return_X_y=True)
+    if y.ndim == 1:
+        y = np.vstack(y)
+    print(name)
+    print("  #observations:", X.shape[0])
+    print("  #features:    ", X.shape[1])
+    print("  #targets:     ", y.shape[1])
+    assert y.shape[0] == X.shape[0]
+    with open(f"{name}.csv", "w") as f:
+        delim = dict(delimiter=",", newline="\n")
+        np.savetxt(f, [X.shape + y.shape[1:]], **delim, fmt="%d")
+        np.savetxt(f, y.T, **delim)
+        np.savetxt(f, X.T, **delim)
