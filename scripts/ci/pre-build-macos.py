e7d41cb65e88a1429bec39465c9952779ea2df94
Pieter Pas <pieter.pas@outlook.com>
2024-02-09 23:49:55 +0100

    [CI] use custom settings_user.yml

--- scripts/ci/pre-build-macos.py
+++ scripts/ci/pre-build-macos.py
@@ -10,21 +10,32 @@ archflags = os.getenv("ARCHFLAGS")
 archs = ()
 if archflags:
     archs = tuple(sorted(re.findall(r"-arch +(\S+)", archflags)))
+
 print("ARCHFLAGS:", archs)
 print("PLATFORM: ", platform.machine())
 print("MACOSX_DEPLOYMENT_TARGET:", os.getenv("MACOSX_DEPLOYMENT_TARGET"))
+print(flush=True)
 
+deployment_target = os.getenv("MACOSX_DEPLOYMENT_TARGET", "10.9")
 can_run = platform.machine() in archs
 conan_arch = {
-    (): None,
-    ("x86_64",): "x86_64",
-    ("arm64",): "armv8.3",
-    ("arm64", "x86_64"): "avr",  # Just to have a distinct architecture
+    (): "ci-native",
+    ("x86_64",): "ci-x86_64",
+    ("arm64",): "ci-arm64",
+    ("arm64", "x86_64"): "ci-universal2",
+}[archs]
+cpu_flags = {
+    (): (),
+    ("x86_64",): ("-march=skylake",),
+    ("arm64",): ("-mcpu=apple-m1",),
+    ("arm64", "x86_64"): (),
 }[archs]
 
-native_profile = """\
+native_profile = f"""\
 include(default)
 [settings]
+arch={conan_arch}
+os.version={deployment_target}
 build_type=Release
 [conf]
 tools.cmake.cmaketoolchain:generator=Ninja Multi-Config
@@ -35,6 +46,7 @@ cross_profile = f"""\
 include(default)
 [settings]
 arch={conan_arch}
+os.version={deployment_target}
 build_type=Release
 [conf]
 tools.cmake.cmaketoolchain:user_toolchain=["{{{{ os.path.join(profile_dir, "cibw.toolchain") }}}}"]
@@ -45,7 +57,10 @@ tools.build:skip_test=True
 
 cross_toolchain = f"""\
 set(CMAKE_SYSTEM_NAME "Darwin")
-set(CMAKE_OSX_ARCHITECTURES "{';'.join(archs)}")
+set(CMAKE_OSX_ARCHITECTURES  "{';'.join(archs)}")
+set(CMAKE_C_FLAGS_INIT       "{' '.join(cpu_flags)}")
+set(CMAKE_CXX_FLAGS_INIT     "{' '.join(cpu_flags)}")
+set(CMAKE_Fortran_FLAGS_INIT "{' '.join(cpu_flags)}")
 """
 
 Path("cibw.profile").write_text(cross_profile if archs else native_profile)
