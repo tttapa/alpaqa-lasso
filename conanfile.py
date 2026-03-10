50b1b3ee7915f18796a4aea86332cb0c3b71d131
Pieter Pas <pieter.p.dev@outlook.com>
2026-03-10 16:38:48 +0100

    Upgrade pybind11

--- conanfile.py
+++ conanfile.py
@@ -18,7 +18,15 @@ class AlpaqaLassoConan(ConanFile):
 
     def configure(self):
         self.requires("alpaqa/1.1.0-alpha.1")
-        self.requires("pybind11/2.13.6")
+        # Note: in theory, this should match the pybind11 version that alpaqa
+        # was built with (which is 2.13.6 for alpaqa 1.1.0-alpha.1), to avoid
+        # ABI issues. However, importing numpy from 2.13.6 causes segfaults in
+        # import_numpy_core_submodule on older versions of Python (e.g. 3.10),
+        # so we use a newer version of pybind11 that doesn't have this issue
+        # (and we cross our fingers that there are no ABI issues).
+        # Future versions of alpaqa will also use newer versions of pybind11,
+        # so this should be fine in the long run.
+        self.requires("pybind11/3.0.1")
         if self.options.with_conan_python:
             self.requires("tttapa-python-dev/3.13.7")
 
